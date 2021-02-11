e3f0c314fb2a7e04ece4744fc312bcde48358747
Henry Pham <HenryPham@live.dk>
2021-02-11 13:36:55 +0100

    added subtraction and divide tests

--- Calculator.Unit.Test/UnitTest1.cs
+++ Calculator.Unit.Test/UnitTest1.cs
@@ -29,13 +29,30 @@ namespace Calculator.Unit.Test
         [Test]
         public void SubtractionTest()
         {
-            //Arrange
-            //var uut = new CalculatorClass();
             //Act
             double result = uut.Subtract(10, 5);
             //Assert
             Assert.That(result, Is.EqualTo(5));
         }
+
+        [Test]
+        public void SubtractionZeroTest()
+        {
+            //Act
+            double result = uut.Subtract(10, 0);
+            //Assert
+            Assert.That(result, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void SubtractionMinusTest()
+        {
+            //Act
+            double result = uut.Subtract(20, -5);
+            //Assert
+            Assert.That(result, Is.EqualTo(25));
+        }
+
 
         [Test]
         public void MultiplyTest()
@@ -46,6 +63,32 @@ namespace Calculator.Unit.Test
             double result = uut.Multiply(8, 5);
             //Assert
             Assert.That(result, Is.EqualTo(40));
+        }
+
+
+        [Test]
+        public void DivideTest()
+        {
+            //Act
+            double result = uut.Divide(10, 5);
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void DivideByZeroTest()
+        {
+            //Act + Assert
+            Assert.That(() => uut.Divide(10, 0), Throws.TypeOf<System.DivideByZeroException>());
+        }
+
+        [Test]
+        public void DivideMinusTest()
+        {
+            //Act
+            double result = uut.Divide(20, -5);
+            //Assert
+            Assert.That(result, Is.EqualTo(-4));
         }
 
         [Test]

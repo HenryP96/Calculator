a4a47988141023cf4067d66f84f25a5ed4954c9e
LucasFriis <lucasfriis123@gmail.com>
2021-02-11 12:47:40 +0100

    Accumaltor

--- Calculator/CalculatorClass.cs
+++ Calculator/CalculatorClass.cs
@@ -12,18 +12,22 @@ namespace Calculator
         }
         public double Add(double a, double b)
         {
+            Accumulator = a + b;
             return a + b;
         }
         public double Subtract(double a, double b)
         {
+            Accumulator = a - b;
             return a - b;
         }
         public double Multiply(double a, double b)
         {
+            Accumulator = a * b;
             return a * b;
         }
         public double Power(double a, double b)
         {
+            Accumulator = Math.Pow(a, b);
             return Math.Pow(a, b);
         }
 
